4bf4ba873ba3d4f9fa3823198aa0334faeb112cd
Teemu Pätsi <teemu@paddy.fi>
2023-03-07 17:48:47 +0200

    Parse function parameters

--- src/ast.rs
+++ src/ast.rs
@@ -1,10 +1,12 @@
 use crate::{
     class::{
         program::Program,
+        signature::{Parameter, Signature},
         statement::{Statement, StatementType},
-        token::{Delimiter, Token, TokenType, Keyword}, signature::Signature,
+        token::{Delimiter, Keyword, Token, TokenType},
     },
     compiler::CompilerError,
+    data_types::{datatype_from_string, DataType},
 };
 
 pub fn generate_ast(tokens: &Vec<Token>) -> Result<Program, CompilerError> {
@@ -36,7 +38,7 @@ fn no_operation_statement(
     tokens: &Vec<Token>,
     cursor: &mut usize,
 ) -> Result<Statement, CompilerError> {
-    advance_cursor(cursor, tokens, TokenType::Delimiter(Delimiter::SemiColon))?;
+    advance_cursor(cursor, tokens, &TokenType::Delimiter(Delimiter::SemiColon))?;
     Ok(Statement {
         typ: StatementType::NoOperation,
         value: None,
@@ -46,12 +48,12 @@ fn no_operation_statement(
 }
 
 fn compound_statement(tokens: &Vec<Token>, cursor: &mut usize) -> Result<Statement, CompilerError> {
-    advance_cursor(cursor, tokens, TokenType::Delimiter(Delimiter::OpenCurly))?;
+    advance_cursor(cursor, tokens, &TokenType::Delimiter(Delimiter::OpenCurly))?;
     let mut statements: Vec<Statement> = Vec::new();
     while tokens[*cursor].typ != TokenType::Delimiter(Delimiter::CloseCurly) {
         statements.push(get_next_statement(tokens, cursor)?);
     }
-    advance_cursor(cursor, tokens, TokenType::Delimiter(Delimiter::CloseCurly))?;
+    advance_cursor(cursor, tokens, &TokenType::Delimiter(Delimiter::CloseCurly))?;
     Ok(Statement {
         typ: StatementType::Compound,
         value: None,
@@ -62,13 +64,16 @@ fn compound_statement(tokens: &Vec<Token>, cursor: &mut usize) -> Result<Stateme
 
 /// fun name(param1: int, param2: str) -> bool { }
 fn function_statement(tokens: &Vec<Token>, cursor: &mut usize) -> Result<Statement, CompilerError> {
-    advance_cursor(cursor, tokens, TokenType::Keyword(Keyword::Fun))?;
-    let function_name: String = advance_cursor(cursor, tokens, TokenType::Identifier)?.value;
-    advance_cursor(cursor, tokens, TokenType::Delimiter(Delimiter::OpenParen))?;
-    // TODO: Parameters
-    advance_cursor(cursor, tokens, TokenType::Delimiter(Delimiter::CloseParen))?;
+    advance_cursor(cursor, tokens, &TokenType::Keyword(Keyword::Fun))?;
+    let function_name: String = advance_cursor(cursor, tokens, &TokenType::Identifier)?.value;
+    advance_cursor(cursor, tokens, &TokenType::Delimiter(Delimiter::OpenParen))?;
+    let parameters: Vec<Parameter> = parse_function_parameters(tokens, cursor)?;
+    advance_cursor(cursor, tokens, &TokenType::Delimiter(Delimiter::CloseParen))?;
     // TODO: Return type
-    let signature: Signature = Signature { parameters: Vec::new(), return_type: Vec::new() };
+    let signature: Signature = Signature {
+        parameters,
+        return_type: None,
+    };
     Ok(Statement {
         typ: StatementType::Function(signature), // TODO: Params, Return type
         value: Some(function_name),
@@ -77,20 +82,62 @@ fn function_statement(tokens: &Vec<Token>, cursor: &mut usize) -> Result<Stateme
     })
 }
 
+fn parse_function_parameters(
+    tokens: &Vec<Token>,
+    cursor: &mut usize,
+) -> Result<Vec<Parameter>, CompilerError> {
+    let mut parameters: Vec<Parameter> = Vec::new();
+    // Parse parameters until ')'
+    while peek_cursor(
+        *cursor,
+        tokens,
+        &TokenType::Delimiter(Delimiter::CloseParen),
+    )
+    .is_err()
+    {
+        parameters.push(parse_next_parameter(tokens, cursor)?);
+    }
+    Ok(parameters)
+}
+
+fn parse_next_parameter(
+    tokens: &Vec<Token>,
+    cursor: &mut usize,
+) -> Result<Parameter, CompilerError> {
+    let name: String = advance_cursor(cursor, tokens, &TokenType::Identifier)?.value;
+    advance_cursor(cursor, tokens, &TokenType::Delimiter(Delimiter::Colon))?;
+    let datatype_str: String = advance_cursor(cursor, tokens, &TokenType::Identifier)?.value;
+    let typ: DataType = datatype_from_string(&datatype_str);
+    // If the next token is a comma, there will be more parameters
+    if peek_cursor(*cursor, tokens, &TokenType::Delimiter(Delimiter::Comma)).is_ok() {
+        *cursor += 1;
+    }
+    Ok(Parameter::new(name, typ))
+}
+
 fn advance_cursor(
     cursor: &mut usize,
     tokens: &Vec<Token>,
-    expected_type: TokenType,
+    expected_type: &TokenType,
 ) -> Result<Token, CompilerError> {
-    if *cursor >= tokens.len() {
+    let token: Token = peek_cursor(cursor.to_owned(), tokens, &expected_type)?;
+    *cursor += 1;
+    Ok(token.clone())
+}
+
+fn peek_cursor(
+    cursor: usize,
+    tokens: &Vec<Token>,
+    expected_type: &TokenType,
+) -> Result<Token, CompilerError> {
+    if cursor >= tokens.len() {
         // TODO: Enhance error reporting
         return Err(CompilerError::ParserError(
             "Unexpected EOF while parsing".to_string(),
         ));
     }
-    let token: &Token = &tokens[*cursor];
-    *cursor += 1;
-    if token.typ != expected_type {
+    let token: &Token = &tokens[cursor];
+    if &token.typ != expected_type {
         return Err(CompilerError::ParserError(format!(
             "Expected TokenType {:?} but got {:?}\n{:?}",
             expected_type, token.typ, token
@@ -102,8 +149,8 @@ fn advance_cursor(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::lexer::tokenize_code;
     use crate::class::statement::StatementType;
+    use crate::lexer::tokenize_code;
 
     #[test]
     fn parse_no_operation_statement() {
@@ -131,10 +178,32 @@ mod tests {
 
     #[test]
     fn parse_function_statement() {
-        let tokens: Vec<Token> = tokenize_code("fun foo() { }", None);
+        let tokens: Vec<Token> = tokenize_code("fun foo(a:int, b:str) { }", None);
         let program: Program = generate_ast(&tokens).expect("Could not generate AST");
         assert_eq!(program.statements.len(), 1);
-        let signature: Signature = Signature { parameters: Vec::new(), return_type: Vec::new() };
-        assert_eq!(program.statements[0].typ, StatementType::Function(signature));
+        let parameters: Vec<Parameter> = vec![
+            Parameter::new("a".to_string(), DataType::Integer),
+            Parameter::new("b".to_string(), DataType::String),
+        ];
+        let return_type: Option<DataType> = None;
+        let signature: Signature = Signature {
+            parameters,
+            return_type,
+        };
+        assert_eq!(
+            program.statements[0].typ,
+            StatementType::Function(signature)
+        );
+        // Function's Statement is always CompoundStatement
+        assert_eq!(
+            program.statements[0]
+                .statements
+                .as_ref()
+                .unwrap()
+                .first()
+                .unwrap()
+                .typ,
+            StatementType::Compound
+        )
     }
 }
--- src/class/signature.rs
+++ src/class/signature.rs
@@ -3,11 +3,20 @@ use crate::data_types::DataType;
 #[derive(Debug, Clone, PartialEq)]
 pub struct Signature {
     pub parameters: Vec<Parameter>,
-    pub return_type: Vec<DataType>,
+    pub return_type: Option<DataType>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Parameter {
     pub name: String,
     pub typ: DataType,
+}
+
+impl Parameter {
+    pub fn new(name: String, typ: DataType) -> Self {
+        Self {
+            name,
+            typ,
+        }
+    }
 }
